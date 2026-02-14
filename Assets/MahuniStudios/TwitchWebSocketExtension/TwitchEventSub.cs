cd9098bfbe106c8de7100ebfedfc8a3a43727eb2
LaMahuni <la@mahuni.com>
2026-02-14 18:52:33 +0100

    - added subscriptions and notification events for locking and ending predictions

--- Assets/MahuniStudios/TwitchWebSocketExtension/TwitchEventSub.cs
+++ Assets/MahuniStudios/TwitchWebSocketExtension/TwitchEventSub.cs
@@ -1,13 +1,13 @@
 // © Copyright 2026 Mahuni Game Studios
 
 // ReSharper disable InconsistentNaming
-
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
-
 namespace Mahuni.Twitch.Extension
 {
     using UnityEngine;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json.Linq;
     
     /// <summary>
     /// Holds subscription request structures needed for the Twitch EventSub
@@ -26,6 +26,9 @@ namespace Mahuni.Twitch.Extension
 
         #region Subscriptions
         
+        /// <summary>
+        /// Base subscription class to inherit from for convenience
+        /// </summary>
         public class TwitchSubscription
         {
             protected JObject jsonObject;
@@ -34,6 +37,8 @@ namespace Mahuni.Twitch.Extension
                 return jsonObject.ToString();
             }
         }
+
+        #region Chat Subscription
         
         // https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelchatmessage
         public class ChatReadSubscription : TwitchSubscription
@@ -58,6 +63,10 @@ namespace Mahuni.Twitch.Extension
                 });
             }
         }
+        
+        #endregion
+
+        #region Poll
         
         // https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelpollbegin
         public class PollBeginSubscription : TwitchSubscription
@@ -84,7 +93,62 @@ namespace Mahuni.Twitch.Extension
         
         #endregion
 
+        #region Prediction Subscription
+        
+        // https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelpredictionlock
+        public class PredictionLockSubscription : TwitchSubscription
+        {
+            public const string subscriptionType = "channel.prediction.lock";
+            public PredictionLockSubscription(string sessionId, string broadcasterUserId)
+            {
+                jsonObject = JObject.FromObject(new
+                {
+                    type = subscriptionType,
+                    version = "1",
+                    condition = new
+                    {
+                        broadcaster_user_id = broadcasterUserId
+                    },
+                    transport = new
+                    {
+                        method = "websocket",
+                        session_id = sessionId
+                    }
+                });
+            }
+        }
+        
+        
+        // https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelpredictionend
+        public class PredictionEndSubscription : TwitchSubscription
+        {
+            public const string subscriptionType = "channel.prediction.end";
+            public PredictionEndSubscription(string sessionId, string broadcasterUserId)
+            {
+                jsonObject = JObject.FromObject(new
+                {
+                    type = subscriptionType,
+                    version = "1",
+                    condition = new
+                    {
+                        broadcaster_user_id = broadcasterUserId
+                    },
+                    transport = new
+                    {
+                        method = "websocket",
+                        session_id = sessionId
+                    }
+                });
+            }
+        }
+        
+        #endregion
+        
+        #endregion
+
         #region Notification Events
+
+        #region Chat Events
 
         //https://dev.twitch.tv/docs/eventsub/eventsub-reference/#channel-chat-message-event
         public class ChatReadEvent
@@ -117,6 +181,80 @@ namespace Mahuni.Twitch.Extension
                 }
             }
         }
+        
+        #endregion
+
+        #region Prediction Events
+        
+        //https://dev.twitch.tv/docs/eventsub/eventsub-reference/#channel-prediction-end-event
+        public class PredictionEndEvent
+        {
+            public readonly string title;
+            public readonly string winningOutcomeId;
+            
+            public PredictionEndEvent(string data)
+            {
+                JObject root = JObject.Parse(data);
+                
+                JToken titleToken = root.SelectToken("title");
+                if (titleToken != null)
+                {
+                    title = titleToken.ToString();
+                }
+                else
+                {
+                    Debug.LogError($"TwitchEventSub: Could not get title from PredictionEndEvent");
+                }
+                
+                JToken winningOutcomeToken = root.SelectToken("winning_outcome_id");
+                if (winningOutcomeToken != null)
+                {
+                    winningOutcomeId = winningOutcomeToken.ToString();
+                }
+                else
+                {
+                    Debug.LogError($"TwitchEventSub: Could not get winning outcome from PredictionEndEvent");
+                }
+              
+                List<JToken> outcomes = root["outcomes"]?.Children().ToList();
+                if (outcomes == null || !outcomes.Any())
+                {
+                    Debug.LogError($"TwitchEventSub: Could not get outcomes array from PredictionEndEvent");
+                }
+                else
+                {
+                    foreach (JToken jToken in outcomes)
+                    {
+                        JObject entry = JObject.Parse(jToken.ToString());
+                        JToken titleToken2 = entry.SelectToken("title");
+                        JToken userToken = entry.SelectToken("users");
+                        Debug.Log($"Outcome with title {titleToken2} and was voted by {userToken} users.");
+                    }
+                }
+            }
+        }
+        
+        //https://dev.twitch.tv/docs/eventsub/eventsub-reference/#channel-prediction-lock-event
+        public class PredictionLockEvent
+        {
+            public readonly string title;
+            public PredictionLockEvent(string data)
+            {
+                JObject root = JObject.Parse(data);
+                
+                JToken titleToken = root.SelectToken("title");
+                if (titleToken != null)
+                {
+                    title = titleToken.ToString();
+                }
+                else
+                {
+                    Debug.LogError($"TwitchEventSub: Could not get title from PredictionLockEvent");
+                }
+            }
+        }
+        
+        #endregion
 
         #endregion
     }
